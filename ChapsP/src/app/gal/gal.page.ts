57d73aa95e806a3b06f87407d293b0094a1e53cd
Santiago Paladines <slpaladines@gmail.com>
2021-04-13 23:12:49 -0500

    Create gal.page.ts

---
+++ ChapsP/src/app/gal/gal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gal',
+  templateUrl: './gal.page.html',
+  styleUrls: ['./gal.page.scss'],
+})
+export class GalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
